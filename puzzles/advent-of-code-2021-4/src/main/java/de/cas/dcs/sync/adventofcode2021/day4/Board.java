4ec6bcd879450715ba5fe39ba6fa7da4bf957736
Julian Obrecht <julian.obrecht@cas.de>
2021-12-04 06:43:01 +0100

    Add day4

---
+++ puzzles/advent-of-code-2021-4/src/main/java/de/cas/dcs/sync/adventofcode2021/day4/Board.java
@@ -0,0 +1,19 @@
+package de.cas.dcs.sync.adventofcode2021.day4;
+
+import java.util.Arrays;
+
+public class Board {
+	int[][] grid = new int[5][5];
+
+	@Override
+	public String toString() {
+		return "Board{" +
+				"grid=" + Arrays.toString(grid) +
+				", matched=" + Arrays.toString(matched) +
+				'}';
+	}
+
+	boolean[][] matched = new boolean[5][5];
+
+
+}
