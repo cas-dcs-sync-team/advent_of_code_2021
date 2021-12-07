63656f6e3131a731018ee76766bfdcafbf7f4095
Julian Obrecht <julian.obrecht@cas.de>
2021-12-07 06:52:10 +0100

    add day 7

---
+++ puzzles/advent-of-code-2021-7/src/main/java/de/cas/dcs/sync/adventofcode2021/day7/PartOne.java
@@ -0,0 +1,45 @@
+package de.cas.dcs.sync.adventofcode2021.day7;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PartOne {
+  private static final Path PUZZLE_RESOURCE = Paths.get("puzzle_resources/day7/day7.file");
+
+  public static void main(String[] args) throws IOException {
+    List<Integer> values =
+        Files.lines(PUZZLE_RESOURCE)
+            .flatMap(s -> Arrays.stream(s.split(",")))
+            .map(Integer::parseInt)
+            .sorted()
+            .collect(Collectors.toList());
+
+    int minPosition = values.get(0);
+    int maxPosition = values.get(values.size() - 1);
+
+    int minFuelCost = Integer.MAX_VALUE;
+    int minFuelCostMeetingPosition = Integer.MAX_VALUE;
+
+    for (int meetingPosition = minPosition; meetingPosition <= maxPosition; meetingPosition++) {
+      int fuelCost = 0;
+      for (Integer crabPosition : values) {
+        fuelCost += Math.abs(meetingPosition - crabPosition);
+      }
+      if (fuelCost < minFuelCost) {
+        minFuelCost = fuelCost;
+        minFuelCostMeetingPosition = meetingPosition;
+      }
+    }
+
+    System.out.println(minFuelCostMeetingPosition + " " + minFuelCost);
+  }
+
+  public static String execute(List<String> values) {
+    return "";
+  }
+}
