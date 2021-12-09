3b1466cf180f25d987f63f8b6eff1857dc096e3b
Julian Neuhaus <julian.neuhaus@cas.de>
2021-12-09 05:55:05 +0100

    add day 9 skeleton

---
+++ puzzles/advent-of-code-2021-9/src/main/java/de/cas/dcs/sync/adventofcode2021/day9/PartTwo.java
@@ -0,0 +1,22 @@
+package de.cas.dcs.sync.adventofcode2021.day9;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class PartTwo {
+  private static final String PUZZLE_RESOURCE_NAME = "day9.file";
+
+  public static void main(String[] args) throws IOException, URISyntaxException {
+    Path path = Paths.get(PartTwo.class.getResource("/" + PUZZLE_RESOURCE_NAME).toURI());
+    Stream<String> lines = Files.lines(path);
+    System.out.println(execute(lines));
+  }
+
+  public static int execute(Stream<String> lines) {
+    return 0;
+  }
+}
