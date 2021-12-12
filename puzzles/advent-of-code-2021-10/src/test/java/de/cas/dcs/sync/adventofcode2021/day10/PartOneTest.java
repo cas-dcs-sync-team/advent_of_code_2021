76ce8ed5c054141f0545de2128bfc4ecc4c5d46a
Julian Neuhaus <neuhaus.julian@gmail.com>
2021-12-12 08:53:34 +0100

    fixed incorrect expected value for part one test of day 10

--- puzzles/advent-of-code-2021-10/src/test/java/de/cas/dcs/sync/adventofcode2021/day10/PartOneTest.java
+++ puzzles/advent-of-code-2021-10/src/test/java/de/cas/dcs/sync/adventofcode2021/day10/PartOneTest.java
@@ -1,6 +1,6 @@
 package de.cas.dcs.sync.adventofcode2021.day10;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -8,15 +8,14 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 class PartOneTest {
 
   @Test
   public void resultTest() throws IOException, URISyntaxException {
     // ARRANGE
-    int expectedResult = 15;
+    int expectedResult = 26397;
     Path path = Paths.get(this.getClass().getResource("/day10.file").toURI());
     Stream<String> lines = Files.lines(path);
 
